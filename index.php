7a48c732deae2b742b4dd7c1b09d233a619e6a2f
Никита <igarev86@gmail.com>
2022-01-22 17:28:35 +0300

    Добавил название
    Написал комментарий

--- index.php
+++ index.php
@@ -2,9 +2,11 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <title>Авторизация и регистрация</title>
 </head>
 <body>
+
+    <!-- Форма авторизации !-->
     
 </body>
 </html>
