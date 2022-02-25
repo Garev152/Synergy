4e787154e663b2585f2aa90e10fbe61f772bba36
Никита <igarev86@gmail.com>
2022-02-25 19:21:30 +0300

    Скопировал авторизацию и вставил в регистрацию

---
+++ register.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Авторизация и регистрация</title>
+    <link rel="stylesheet" href="assets/css/main.css">
+</head>
+<body>
+
+    <!-- Форма авторизации !-->
+
+    <form action="" method="">
+        <label>Логин</label>
+        <input type="text" placeholder="Введите свой e-mail">
+        <label>Пароль</label>
+        <input type="password" placeholder="Введите пароль">
+        <button>Войти</button>
+        <p>
+            У вас не аккаунта? - <a href="#">зерегистрируйтесь</a>!
+        </p>
+    </form>
+    
+</body>
+</html>
