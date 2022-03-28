39449609acb40d73912bd256dca5ed6fbb1d7b03
Никита <igarev86@gmail.com>
2022-03-29 00:26:40 +0300

    корректирую сайт

--- register.php
+++ register.php
@@ -1,8 +1,8 @@
 <?php
-session_start();
-if (!$_SESSION['user']) {
-    header('Location: profile.php');
-}
+    session_start();
+    if ($_SESSION['user']) {
+        header('Location: profile.php');
+    }
 ?>
 
 <!DOCTYPE html>
--- vendor/signup.php
+++ vendor/signup.php
@@ -14,5 +14,48 @@
 
     mysqli_query($connect, "INSERT INTO `users`(`id`, `full_name`, `email`, `series`, `number`, `who_issued`, `date_of_issue`, `password`) VALUES (NULL, '$full_name', '$email', '$series', '$number', '$who_issued', '$date_of_issue', '$password')");
 
+    $error_fields = [];
+
+    if ($full_name === '') {
+        $error_fields[] = 'full_name';
+    }
+    
+    if ($password === '') {
+        $error_fields[] = 'password';
+    }
+    
+    if ($series === '') {
+        $error_fields[] = 'series';
+    }
+    
+    if ($email === '' || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $error_fields[] = 'email';
+    }
+    
+    if ($number === '') {
+        $error_fields[] = 'number';
+    }
+    
+    if ($who_issued === '') {
+        $error_fields[] = 'who_issued';
+    }
+
+    if ($date_of_issue === '') {
+        $error_fields[] = 'date_of_issue';
+    }
+    
+    if (!empty($error_fields)) {
+        $response = [
+            "status" => false,
+            "type" => 1,
+            "message" => "Проверьте правильность полей",
+            "fields" => $error_fields
+        ];
+    
+        echo json_encode($response);
+    
+        die();
+    }
+
     $_SESSION['message']='Регистрация прошла успешно!';
     header('Location: ../index.php');
